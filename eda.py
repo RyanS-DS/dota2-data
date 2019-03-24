fc2d9d136855c74ebce6dc30dab4549ca736335b
RyanS-DS <48695526+RyanS-DS@users.noreply.github.com>
2019-03-24 15:59:39 +0000

    Quick EDA and stored dataset to csv

--- eda.py
+++ eda.py
@@ -7,7 +7,27 @@ Created on Sun Mar 24 14:36:37 2019
 
 import pandas as pd
 from pathlib import Path
+import numpy as np
 
 base_folder = Path('C:/Users/Ryan/dota2-classification')
 
 df = pd.read_csv(base_folder / 'matches.csv')
+
+df.head()
+
+#top10 = df.groupby('hero_name').size().nlargest(10).index.values.tolist()
+#
+#rdf = df[df['hero_name'].isin(top10)]
+#
+#rdf.groupby('hero_name').agg({'kills':np.mean, 'assists':np.mean, 'deaths':np.mean})
+
+
+top3 = df.groupby('hero_name').size().nlargest(3).index.values.tolist()
+
+rdf = df[df['hero_name'].isin(top3)]
+
+rdf.groupby('hero_name').agg({'kills':np.mean, 'assists':np.mean, 'deaths':np.mean})
+
+rdf = rdf[['hero_name', 'kills', 'assists', 'deaths']]
+
+rdf.to_csv(base_folder  / 'dataset.csv', index = False)
